912ed7e2fd6d9b8cc3356afb74734860af6dd5c6
kunal-agr <kunalkagrawal2004@gmail.com>
2026-01-31 22:47:41 +0530

    Dockerize application and refactor JdbcUtils to use environment variables

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# ---------- BUILD STAGE ----------
+FROM maven:3.9.9-eclipse-temurin-21 AS build
+
+WORKDIR /app
+
+COPY pom.xml .
+RUN mvn dependency:go-offline
+
+COPY src ./src
+RUN mvn clean package -DskipTests
+
+
+# ---------- RUNTIME STAGE ----------
+FROM tomcat:11.0-jdk21-temurin
+
+RUN rm -rf /usr/local/tomcat/webapps/*
+
+COPY --from=build /app/target/crudApp.war /usr/local/tomcat/webapps/crudApp.war
+
+EXPOSE 8080
+
+CMD ["catalina.sh", "run"]
--- src/main/java/com/kagrawal/crudapp/utils/JDBCUtils.java
+++ src/main/java/com/kagrawal/crudapp/utils/JDBCUtils.java
@@ -5,15 +5,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCUtils {
-    private static final String URL = "jdbc:postgresql://localhost:5432/cruddb";
-    private static final String USER =  "postgres";
-    private static final String PASS = "password";
+
+    private static final String URL =
+            System.getenv("DB_URL") != null
+                    ? System.getenv("DB_URL")
+                    : "jdbc:postgresql://" +
+                    System.getenv("DB_HOST") + ":" +
+                    System.getenv("DB_PORT") + "/" +
+                    System.getenv("DB_NAME");
+
+
+    private static final String USER =
+            System.getenv("DB_USER") != null
+                    ? System.getenv("DB_USER")
+                    : "postgres";
+
+    private static final String PASS =
+            System.getenv("DB_PASSWORD") != null
+                    ? System.getenv("DB_PASSWORD")
+                    : "password";
 
     static {
         try {
             Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException cnfe) {
-            cnfe.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("PostgreSQL Driver not found", e);
         }
     }
 
